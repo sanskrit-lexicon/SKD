d3cec41ae3fa06f30f708bd8208af96044af7f65
funderburkjim <funderburkjim@gmail.com>
2020-05-18 14:15:30 -0400

    Refactor so redo.sh works in this directory on local xampp installation

---
+++ verbs01/redo.sh
@@ -0,0 +1,19 @@
+echo "make mwverbs"
+# In local installation, this directory is
+# /c/xampp/htdocs/sanskrit-lexicon/skd/verbs01
+# And csl-orig directory is
+# /c/xampp/htdocs/cologne/csl-orig
+# Thus csl-orig relative location relative to this directory is
+CSLORIG="../../../cologne/csl-orig"
+python mwverb.py mw ${CSLORIG}/v02/mw/mw.txt mwverbs.txt
+echo "make mwverbs1"
+python mwverbs1.py mwverbs.txt mwverbs1.txt
+echo "make skd_verb_filter"
+python skd_verb_filter.py ${CSLORIG}/v02/skd/skd.txt skd_verb_filter.txt
+echo "make skd_verb_filter_map"
+python skd_verb_filter_map.py skd_verb_filter.txt vcp_mw_map_init.txt mwverbs1.txt skd_verb_filter_map.txt
+echo "make skd_verb1"
+python verb1.py slp1 ${CSLORIG}/v02/skd/skd.txt skd_verb_filter_map.txt skd_verb1.txt
+echo "make skd_verb1_deva"
+python verb1.py deva ${CSLORIG}/v02/skd/skd.txt skd_verb_filter_map.txt skd_verb1_deva.txt
+echo "REDO.SH done"
