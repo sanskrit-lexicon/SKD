10cf830bdd1a01f49347531a67258cbedd408a36
funderburkjim <funderburkjim@gmail.com>
2022-06-06 22:34:02 -0400

    #15

--- corrections/issue15/readme.txt
+++ corrections/issue15/readme.txt
@@ -1,3 +1,50 @@
 Corrrections discussed in issue 15:
  https://github.com/sanskrit-lexicon/SKD/issues/15
 Modify pc in metalines to include column data.
+
+-------------------------------------------------------------
+temp_skd_0.txt
+local copy of latest skd.txt from csl-orig repository
+Using the local installation conventions of ejf:
+cp /c/xampp/htdocs/cologne/csl-orig/v02/skd/skd.txt temp_skd_0.txt
+
+-------------------------------------------------------------
+SKD.pc.values.in.metalines.txt
+provided by Andhrabharati
+42196 lines
+each line tab-delimited with 3 columns:
+Sample:
+<L>1	<pc>1-001	[1-001-a]
+<L>2	<pc>1-001	[1-001-a]
+
+-------------------------------------------------------------
+Objective:  change the pc element of metaline according to the above.
+Example for L=1
+old metaline:
+<L>1<pc>1-001<k1>a<k2>a
+new metaline:
+<L>1<pc>1-001-a<k1>a<k2>a
+-------------------------------------------------------------
+# make change_1.txt,
+# the change transactions to apply to temp_skd_0.txt
+# Note This is fairly slow, due to use of linear search in
+# get_pcrec_for_entry function. On my fast pc it took about a minute.
+
+python test_make_change_pc.py temp_skd_0.txt SKD.pc.values.in.metalines.txt change_1.txt
+
+-------------------------------------------------------------
+temp_skd_1.txt  Apply changes
+python updateByLine.py temp_skd_0.txt change_1.txt temp_skd_1.txt
+580774 lines read from temp_skd_0.txt
+580774 records written to temp_skd_1.txt
+42196 change transactions from change_1.txt
+42196 of type new
+
+-------------------------------------------------------------
+temp_skd_1.txt  now ready for replacing  csl-orig/v02/skd/skd.txt
+=============================================================
+
+Next exercise: See how to replace the slow get_pcrec_for_entry
+function with a much faster method
+using Python dictionary.
+
--- corrections/issue15/test_make_change_pc.py
+++ corrections/issue15/test_make_change_pc.py
@@ -7,15 +7,33 @@ import digentry
 
 class Pcerror(object):
   def __init__(self,line):
+   # sample line: tab-delimited 3 fields
+   # <L>1	<pc>1-001	[1-001-a]
    # dummy property values
    self.line = line
-   self.oldmetaline = re.sub(r"(:<pc>.*$)|(\s)", "", line)
-   self.newpc = re.sub(r"<L>.+:<pc>", "", line)
-   self.newmetaline = re.sub(r"<pc>.+<k1>", "<pc>" + self.newpc + "<k1>", self.oldmetaline)
-
+   self.col1,self.col2,self.col3 = line.split('\t') # array with three parts
+   # Retrieve L-value from col1
+   m = re.search(r'^<L>([^ ]*)$',self.col1)
+   if m == None:
+     print('Pcerror. Error parsing col 1')
+     exit(1)
+   self.L = m.group(1) # eg. the '1' from '<L>1'
+   # retrieve old <pc> value from col2
+   m = re.search(r'^<pc>([^ ]*)$',self.col2)
+   if m == None:
+     print('Pcerror. Error parsing col 2')
+     exit(1)
+   self.oldpc = m.group(1)  # eg. '1-001'
+   # retrieve newe <pc> value from col3
+   m = re.search(r'^\[([^ ]*)\]$',self.col3)
+   if m == None:
+     print('Pcerror. Error parsing col 3')
+     exit(1)
+   self.newpc = m.group(1)  # eg. '1-001-a'
+   
 def init_pcrecs(filein):
  recs=[]  # list of Pcerror objects, to be returned
- dbg = True
+ dbg = False
  with codecs.open(filein,encoding='utf-8',mode='r') as f:
   for line in f:
    line = line.rstrip('\r\n') # remove line-ending character(s)
@@ -26,9 +44,10 @@ def init_pcrecs(filein):
   for i in range(0,3):
    rec = recs[i]
    print('record',i+1)  # why +1 ?
-   print(' oldmetaline = "%s"' % rec.oldmetaline)
+   print(' L = "%s"' % rec.L)
+   print(' oldpc       = "%s"' % rec.oldpc)
    print(' newpc       = "%s"' % rec.newpc)
-   print(' newmetaline = "%s"' % rec.newmetaline)
+ 
  return recs
            
 class Change(object):
@@ -37,28 +56,66 @@ class Change(object):
   self.pcrec = pcrec
   
 def get_pcrec_for_entry(entry,pcrecs):
+ """ Match on L
+  Entry object has a dictionary entry.metad, by which L may be found
+  
+ """
+ L = entry.metad['L']
  for pcrec in pcrecs:
-  if pcrec.oldmetaline == entry.metaline:
+  if pcrec.L == L:
    return pcrec
  # find which pcrec matches entry, and return that pcrec.
  # If no match is found, return None
  return None
 
 def generate_changes(entries,pcrecs):
+ """
+  For each entry, we look at metaline.
+    Example: <L>99<pc>1-004<k1>aklikA<k2>aklikA
+  1. Find the Pcerror record with the same 'L'.
+    example: <L>96	<pc>1-004	[1-004-b]
+  2. Check that the pc value from metaline agrees with oldpc
+  3. Replace oldpc with newpc in metaline:
+    example: <L>99<pc>1-004-b<k1>aklikA<k2>aklikA
+  4. generate a Change object
+  Return the array of Change objects
+
+  How to do step 1?  One way is a 'linear search':
+  In our example, metaline L = 99
+  for pcrec in pcrecs:
+   if pcrec.L == L:
+    return pcrec
+  return None  # no match
+
+  This works, but is slow.
+   there are 42000+ records in pcrecs
+  And this search must be done for each of the 42000 entries of skd.
+  So roughly we have 42000*42000  
+   (* 42000 42000) 1,764,000,000  almost 2 billion
+ """
  changes = [] # computed by this function
  for entry in entries:
   pcrec = get_pcrec_for_entry(entry,pcrecs)
   if pcrec != None:
+   # check that pc from entry is same as oldpc from pcrec
+   metapc = entry.metad['pc']
+   if metapc != pcrec.oldpc:
+    print('generate_changes: Mismatch between metpc and oldpc')
+    print('  metapc = ',metapc)
+    print('   oldpc = ',oldpc)
+    continue # don't make a change object
    # generate a change object 
    change = Change(entry,pcrec)
    changes.append(change)
+  else:
+   print('Could not find pcrec for entry',entry.metaline)
  print(len(changes),'lines that may need changes')
  return changes
 
 def get_title(pcrecs):
  outarr = []
  outarr.append('; ===================================================')
- outarr.append('; PC (page-column) ERRORS correction')
+ outarr.append('; PC (page-column) changes')
  outarr.append('; ===================================================')
  return outarr
                
@@ -70,12 +127,11 @@ def write_changes(fileout,changes,title):
   entry = change.entry
   pcrec = change.pcrec
   outarr.append('; -------------------------------------')
-  metaline = entry.metaline
-  metaline1 = re.sub(r'<k2>.*$','',metaline)  # just show L,pc,k1
-  oldmetaline = pcrec.oldmetaline
-  newmetaline = pcrec.newmetaline
-  lnum = entry.linenum1
-  outarr.append('; %s' %metaline1)
+  oldmetaline = entry.metaline
+  # replace <pc>X< in metaline with Y = newpc
+  newpc = pcrec.newpc
+  newmetaline = re.sub(r'<pc>(.*?)<','<pc>%s<' %newpc,oldmetaline)
+  lnum = entry.linenum1  # line number of metaline in skd.txt
   outarr.append('%s old %s' %(lnum,oldmetaline))
   outarr.append(';')
   outarr.append('%s new %s' %(lnum,newmetaline))
@@ -87,10 +143,11 @@ def write_changes(fileout,changes,title):
  print(len(outrecs),"records written to",fileout)
 
 if __name__=="__main__":
- filein = sys.argv[1] #  xxx.txt
- filein1 = sys.argv[2]  # pcerrors.txt
+ filein = sys.argv[1] # skd.txt
+ filein1 = sys.argv[2]  # skd.pc.values ... f
  fileout = sys.argv[3] # changes
  entries = digentry.init(filein)
+ # entries = entries[:100]  # first 100 for testing
  pcrecs = init_pcrecs(filein1)
  changes = generate_changes(entries,pcrecs)
  title = get_title(pcrecs)
